304d00a62b87a1c4bd9ab1fdb47d8f7fed63fcd3
Obo Agboghidi <oagboghi2@gmail.com>
2018-06-15 10:25:10 -0700

    Obo Agboghidi: Working on password hashing.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,10 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User.create(req.body)
+    .then(({username, password}) => {
+      res.status(201).json({ username, password })
+    })
 };
 
 module.exports = {
